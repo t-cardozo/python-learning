abb83bb1613cc6842ca3564ec11fbdae43da0f4c
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 00:52:32 +1000

    area function

--- py-input.py
+++ py-input.py
@@ -1,4 +1,6 @@
 #Inputs:
-name = input('What is your name:')
-age = input('...and your age?')
-print('Hi '+ name)
+#Get area of circle:
+
+radius = input('Enter the radius of your circle (m): ')
+area = 3.14*radius**2
+print('The area of your circle is:',area)

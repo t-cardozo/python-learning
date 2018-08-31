308c4d8fc3e9c312a51cb32693bb54e7d288c5be
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 01:13:11 +1000

    string if

--- py-if-statement.py
+++ py-if-statement.py
@@ -1,9 +1,17 @@
-age = int(input('Enter your age please:'))
+# age = int(input('Enter your age please:'))
 
-if age < 10:
-    #code block
-    print('you are young!')
-elif age < 40:
-    print('under 40')
+# if age < 10:
+#     #code block
+#     print('you are young!')
+# elif age < 40:
+#     print('under 40')
+# else:
+#     print('huh?')
+
+
+meaty = input('Do you eat meat? (y/n):')
+
+if meaty == 'y':
+    print('meat menu')
 else:
-    print('huh?')
+    print('veggie menu')

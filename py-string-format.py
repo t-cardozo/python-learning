26cb1504c8f1e5063b7555c3b845b10225d0fe16
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 00:59:38 +1000

    method 2

--- py-string-format.py
+++ py-string-format.py
@@ -2,4 +2,7 @@ num1 = 3.142494
 num2 = 10.4123
 
 #Format Method #1
-print('num 1 is {0:.3f} and num 2 is {1:.3}'.format(num1, num2))
+#print('num 1 is {0:.3f} and num 2 is {1:.3}'.format(num1, num2))
+
+#Format Method #2
+print(f'num1 is {num1:.4f} and num 2 is {num2:.2f}')

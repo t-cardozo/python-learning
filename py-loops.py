44d77470ad415a0dfb8bf42d127fe0c90b1e9841
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 21:31:15 +1000

    if loops

--- py-loops.py
+++ py-loops.py
@@ -3,5 +3,11 @@ numbers = ['one', 'two', 'three', 'four', 'five']
 # for number in numbers:
 #     print(number)
 
-for number in numbers[1:3]:
-    print(number)
+# for number in numbers[1:3]:
+#     print(number)
+
+for number in numbers:
+    if number == numbers[3:3]:
+        print(f'{number} - selected')
+        else:
+        print(number)

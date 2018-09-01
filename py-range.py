31e511332be1d1250fd3c954017e6a9aa1696c64
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 23:54:24 +1000

    range with string

--- py-range.py
+++ py-range.py
@@ -2,6 +2,12 @@
 #     # do something
 #     print(n)
 
-for n in range(0,10,4):
-    # do something
-    print(n)
+# for n in range(0,10,4):
+#     # do something
+#     print(n)
+
+
+burgers = ['beef', 'chicken', 'veg', 'classic', 'double']
+
+for n in range(len(burgers)):
+    print(n, burgers[n])

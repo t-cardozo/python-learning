b555c8208429634a18a959d0a3b627c7a93f9f50
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 23:56:21 +1000

    backwards range

--- py-range.py
+++ py-range.py
@@ -9,5 +9,8 @@
 
 burgers = ['beef', 'chicken', 'veg', 'classic', 'double']
 
-for n in range(len(burgers)):
+# for n in range(len(burgers)):
+#     print(n, burgers[n])
+
+for n in range(len(burgers)-1, -1, -1):
     print(n, burgers[n])

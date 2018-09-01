6d4e9330446f3f8520ed00bbf3294e56d00795cb
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-01 21:36:49 +1000

    while

--- py-loops.py
+++ py-loops.py
@@ -1,13 +1,24 @@
 numbers = ['one', 'two', 'three', 'four', 'five']
 
+#FOR LOOPS:
+
 # for number in numbers:
 #     print(number)
 
 # for number in numbers[1:3]:
 #     print(number)
 
-for number in numbers:
-    if number == numbers[3]:
-        print(f'{number} - selected')
-    else:
-        print(number)
+# for number in numbers:
+#     if number == numbers[3]:
+#         print(f'{number} - selected')
+#     else:
+#         print(number)
+
+#WHILE LOOPS:
+
+age = 25
+num = 0
+
+while num < age:
+    print(num)
+    

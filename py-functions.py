7bbac22a10221c8ee011618d40943ffdf23e4d67
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-02 00:10:29 +1000

    functions

---
+++ py-functions.py
@@ -0,0 +1,7 @@
+def greet(name='Laura', time = 'Morning'):
+    print(f'Good {time} {name}, hope you are well!')
+
+# name = input('enter your name')
+# time = input('enter the time of the day:')
+
+greet()

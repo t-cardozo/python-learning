898e5e296bd68385c21c4b4993609e64d84cb53b
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-02 00:23:55 +1000

    area vol

--- py-functions.py
+++ py-functions.py
@@ -1,7 +1,19 @@
-def greet(name='Laura', time = 'Morning'):
-    print(f'Good {time} {name}, hope you are well!')
+# def greet(name='Laura', time = 'Morning'):
+#     print(f'Good {time} {name}, hope you are well!')
 
-# name = input('enter your name')
-# time = input('enter the time of the day:')
+# # name = input('enter your name')
+# # time = input('enter the time of the day:')
 
-greet('Troy', 'night')
+# greet('Troy', 'night')
+
+def area(radius):
+    return 3.142 * radius * radius
+
+def vol(area, length):
+    print(area * length)
+
+radius = int(input('enter a radius:'))
+length = int(input('enter a length:'))
+
+area_calcuation = area(radius)
+vol(area_calcuation, length)

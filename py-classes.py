bee3f168a12642aa350a73f42f74a077952a8386
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-03 23:25:43 +1000

    attach param to method

--- py-classes.py
+++ py-classes.py
@@ -1,10 +1,10 @@
 class Planet:
 
-    def __init__(self):
-        self.name = 'Juan'
-        self.radius = 30000
-        self.gravity = 5.5
-        self.system = 'Juan System'
+    def __init__(self, name = 'Juan', radius = 30000, gravity = 5.5, system = 'Juan System'):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
 
     def orbit(self):
         return f'{self.name} is orbiting in the {self.system}'
@@ -13,3 +13,5 @@ juan_planet = Planet()
 
 print(f'Name is {juan_planet.name}')
 print(juan_planet.orbit())
+
+plantX = Planet()

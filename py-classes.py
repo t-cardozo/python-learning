ac33f7cc78f776aebe2bcea862a460de8311dd27
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-03 23:23:40 +1000

    attach method

--- py-classes.py
+++ py-classes.py
@@ -6,7 +6,10 @@ class Planet:
         self.gravity = 5.5
         self.system = 'Juan System'
 
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
 
 juan_planet = Planet()
 
 print(f'Name is {juan_planet.name}')
+print(juan_planet.orbit())

7a3fc83b1795d3c5dc87a0533543d07535189fe4
t-cardozo <t-cardozo@users.noreply.github.com>
2018-09-03 23:21:43 +1000

    classes

---
+++ py-classes.py
@@ -0,0 +1,12 @@
+class Planet:
+
+    def __init__(self):
+        self.name = 'Juan'
+        self.radius = 30000
+        self.gravity = 5.5
+        self.system = 'Juan System'
+
+
+juan_planet = Planet()
+
+print(f'Name is {juan_planet.name}')
